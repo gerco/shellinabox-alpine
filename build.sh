fb43a02b8dab2155d1e089de43a8b480182d721a
Gerco Dries <gerco@gdries.nl>
2015-09-29 20:37:33 -0400

    Pushed to docker hub

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker build -t gdries/shellinabox-alpine-build -f Dockerfile.build .
-docker run --rm gdries/shellinabox-alpine-build > shellinabox.tar.gz
+docker build -t gerco/shellinabox-alpine-build -f Dockerfile.build .
+docker run --rm gerco/shellinabox-alpine-build > shellinabox.tar.gz
 
-docker build -t gdries/shellinabox-alpine .
+docker build -t gerco/shellinabox-alpine .
